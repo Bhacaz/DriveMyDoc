7d03022cbc3da1c5286f84f7d5f58c06ee750c84
Jean-Francis Bastien <bhacaz@gmail.com>
2019-05-28 23:00:41 -0400

    Login and redirect

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,24 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, NgZone, OnInit} from '@angular/core';
+import { Router } from '@angular/router';
+
+declare const gapi: any;
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  user: any;
+  auth2: any;
+
+  private clientId: string = '798021972045-vo31kgilujafp0q9gibdmfe7jr5rau25.apps.googleusercontent.com';
+  private scope = [
+    'profile',
+    'email',
+    'https://www.googleapis.com/auth/drive.readonly'
+  ].join(' ');
+
+  constructor(private router: Router, private ngZone: NgZone) { }
 
   ngOnInit() {
 
   }
 
-  onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  ngAfterViewInit(){
+    this.googleInit();
+  }
+
+  googleInit() {
+    gapi.load('auth2', () => {
+      this.auth2 = gapi.auth2.init({
+        client_id: this.clientId,
+        cookiepolicy: 'single_host_origin',
+        scope: this.scope
+      });
+      this.attachSignin(document.getElementById('googleBtn'));
+    });
+  }
+
+  attachSignin(element) {
+    this.auth2.attachClickHandler(element, {},
+      (googleUser) => {
+
+        let profile = googleUser.getBasicProfile();
+        this.user = {};
+        this.user.token = googleUser.getAuthResponse().access_token;
+        this.user.email = profile.getEmail();
+        this.user.name = profile.getName();
+        this.user.imageUrl = profile.getImageUrl();
+        console.log(this.user);
+        this.ngZone.run(() => this.router.navigate(['/']));
+
+      }, (error) => {
+        alert(JSON.stringify(error, undefined, 2));
+      });
   }
 
 }
